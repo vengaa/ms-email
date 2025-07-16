34e92d97ff25c47ca1de05fed89fc036f9c09a58
Luis Felipe Pereira de Lima <luislima@plurismidia.com.br>
2025-07-16 17:50:59 -0300

    feat: created enums for StatusEmail

--- src/main/java/com/ms/email/enums/StatusEmail.java
+++ src/main/java/com/ms/email/enums/StatusEmail.java
@@ -1,9 +1,16 @@
 package com.ms.email.enums;
 
 public enum StatusEmail {
+    PENDING(0, "S"),
+    ERROR(1, "N"),
+    SENT(2, "T");
 
-    PENDING,
-    SENT,
-    ERROR
 
+    private final int code;
+    private final String description;
+
+    StatusEmail(int code, String description) {
+        this.code = code;
+        this.description = description;
+    }
 }

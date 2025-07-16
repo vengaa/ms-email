808d95eaf96341f182ab6b526efb11f2261dc5fa
Luis Felipe Pereira de Lima <luislima@plurismidia.com.br>
2025-07-16 17:49:53 -0300

    chore: created controller email

---
+++ src/main/java/com/ms/email/controllers/EmailController.java
@@ -0,0 +1,29 @@
+package com.ms.email.controllers;
+
+import com.ms.email.dtos.EmailDto;
+import com.ms.email.models.EmailModel;
+import com.ms.email.services.EmailService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @PostMapping("/sending-email")
+    public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDto emailDto) {
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+        return new ResponseEntity<>(emailModel, HttpStatus.OK);
+    }
+
+}

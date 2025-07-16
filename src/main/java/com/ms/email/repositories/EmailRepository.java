4fa5f2316d44d1c722a2e3ca0f932550bef9d834
Luis Felipe Pereira de Lima <luislima@plurismidia.com.br>
2025-07-16 17:50:15 -0300

    feat: created EmailRepository

---
+++ src/main/java/com/ms/email/repositories/EmailRepository.java
@@ -0,0 +1,11 @@
+package com.ms.email.repositories;
+
+import com.ms.email.enums.StatusEmail;
+import com.ms.email.models.EmailModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmailRepository extends JpaRepository<EmailModel, Long> {
+    List<EmailModel> findByStatusEmail(StatusEmail statusEmail);
+}
